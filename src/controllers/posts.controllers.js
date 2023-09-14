ff4952a5377314c4ccddbfaa35b01a51acc886e7
Cerebro2005 <almeida_martin@hotmail.com>
2023-09-13 21:38:09 -0300

    Subiendo Archivos

---
+++ src/controllers/posts.controllers.js
@@ -0,0 +1,62 @@
+const { PostModel } = require("../models/Post");
+
+// controladores que renderizan páginas
+const getAllPosts = async (req, res) => {
+  const allPosts = await PostModel.findAll();
+
+  res.render("index", { allPosts });
+};
+
+const formCreateNewPost = async (req, res) => {
+  res.render("new-post");
+};
+
+const formUpdatePost = async (req, res) => {
+  const postId = req.params.id;
+
+  const post = await PostModel.findByPk(postId);
+
+  if (!post) {
+    return res.redirect("/posts");
+  }
+
+  res.render("update-post", { post });
+};
+
+// controladores que efectuan cambios en la base de datos y redireccionan
+const createPost = async (req, res) => {
+  const { title, content, imageUrl } = req.body;
+
+  await PostModel.create({ title, content, imageUrl});
+
+  res.redirect("/posts");
+};
+
+const updatePost = async (req, res) => {
+  const { id, title, content, imageUrl } = req.body;
+
+  const post = await PostModel.findByPk(id);
+
+  await post.update({ title, content, imageUrl });
+
+  res.redirect("/posts");
+};
+
+const deletePost = async (req, res) => {
+  const postId = req.params.id;
+
+  const post = await PostModel.findByPk(postId);
+
+  await post.destroy();
+
+  res.redirect("/posts");
+};
+
+module.exports = {
+  getAllPosts,
+  formCreateNewPost,
+  createPost,
+  formUpdatePost,
+  updatePost,
+  deletePost,
+};
